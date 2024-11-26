066de423f857d14be4d40447b00676060904b53b
DimasArya1405 <dimasaryad55@gmail.com>
2024-11-26 10:28:32 +0700

    Mengubah routes

--- routes.php
+++ routes.php
@@ -3,25 +3,25 @@
 
 require_once 'app/controllers/EventsController.php';
 
-$controller = new EventsController();
+$eventscontroller = new EventsController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($url == '/events/index' || $url == '/') {
-    $controller->index();
+    $eventscontroller->index();
 } elseif ($url == '/events/create' && $requestMethod == 'GET') {
-    $controller->create();
+    $eventscontroller->create();
 } elseif ($url == '/events/store' && $requestMethod == 'POST') {
-    $controller->store();
+    $eventscontroller->store();
 } elseif (preg_match('/\/events\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->edit($userId);
+    $eventscontroller->edit($userId);
 } elseif (preg_match('/\/events\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
     $userId = $matches[1];
-    $controller->update($userId, $_POST);
+    $eventscontroller->update($userId, $_POST);
 } elseif (preg_match('/\/events\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->delete($userId);
+    $eventscontroller->delete($userId);
 } else {
     http_response_code(404);
     echo "404 Not Found";
