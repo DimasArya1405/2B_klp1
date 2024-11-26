7e97720b0d0ef886e455c648c0c0e772d21c847e
NoniAprillia <noniclp02@gmail.com>
2024-11-26 10:33:28 +0700

    perubahan di routes

--- routes.php
+++ routes.php
@@ -3,25 +3,25 @@
 
 require_once 'app/controllers/SponsorshipsController.php';
 
-$controller = new SponsorshipsController();
+$sponsorshipsController = new SponsorshipsController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($url == '/sponsorships/index' || $url == '/') {
-    $controller->index();
+    $sponsorshipsController->index();
 } elseif ($url == '/sponsorships/create' && $requestMethod == 'GET') {
-    $controller->create();
+    $sponsorshipsController->create();
 } elseif ($url == '/sponsorships/store' && $requestMethod == 'POST') {
-    $controller->store();
+    $sponsorshipsController->store();
 } elseif (preg_match('/\/sponsorships\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->edit($userId);
+    $sponsorshipsController->edit($userId);
 } elseif (preg_match('/\/sponsorships\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
     $userId = $matches[1];
-    $controller->update($userId, $_POST);
+    $sponsorshipsController->update($userId, $_POST);
 } elseif (preg_match('/\/sponsorships\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->delete($userId);
+    $sponsorshipsController->delete($userId);
 } else {
     http_response_code(404);
     echo "404 Not Found";
